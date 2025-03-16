d92c02334d8c8868621e0c05a66cebfa7ec2ce17
Rajesh Shah <raj@12nines.com>
2025-03-16 21:35:39 +0000

    updated mason config

--- lua/rajshahuk/lazy/lsp.lua
+++ lua/rajshahuk/lazy/lsp.lua
@@ -27,8 +27,7 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
-                "gopls",
+                "rust_analyzer"
             },
             handlers = {
                 function(server_name) -- default handler (optional)
@@ -81,7 +80,7 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
